ccdf55032313dab2a868a213e47c928973e68c08
palazzid88 <palazzid88@gmail.com>
2025-08-30 20:29:55 -0300

    se actualiza sanity.cli

--- sanity.cli.js
+++ sanity.cli.js
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'gn5d09bw',
     dataset: 'iglesia'
   },
+    studioHost: 'iglesia-project', // 👈 esta línea fija el dominio
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
